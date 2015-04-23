b8ff99bf97937e23cf0a2690eb670afdff322ca7
KYS <project@LGD-A15>
2015-04-23 15:44:36 +0900

    delete v0.3

--- when2meet_v0.3/src/main/java/com/lg/team3/dao/MemberScheduleDao.java
+++
@@ -1,9 +0,0 @@
-package com.lg.team3.dao;
-
-import org.mybatis.spring.support.SqlSessionDaoSupport;
-
-public class MemberScheduleDao extends SqlSessionDaoSupport {
-	private final String namespace = "com.lg.team3.memberSchedule.";
-
-	
-}
